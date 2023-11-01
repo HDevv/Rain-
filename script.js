9713a31e82db1dd719bbbfc49ebd4e51e6f3a2a1
Hicham Roldan <82762193+Hicham017@users.noreply.github.com>
2023-11-01 13:43:47 +0100

    ajout de commentaires

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
+// on fait tomber la pluie sur tout le body du HTML
 const body = document.body;
-
 
 // Pluie
 setInterval(rainFall, 10);
 
-function rainFall(){
-    const waterDrop = document.createElement('i');
-    
+function rainFall() {
+  const waterDrop = document.createElement("i");
 
-    // Récupère l'icone (pas besoin de le mettre dans le HTML)
-    waterDrop.classList.add('fas');
-    waterDrop.classList.add('fa-tint');
+  // Récupère l'icone (pas besoin de le mettre dans le HTML)
+  waterDrop.classList.add("fas");
+  waterDrop.classList.add("fa-tint");
 
-    // On injecte le CSS via le JS
-    waterDrop.style.fontSize = Math.random() * 7 + 'px';
-    waterDrop.style.animationDuration = Math.random() * 2 + 's';
-    waterDrop.style.opacity = Math.random() + 0.3;
+  // On injecte le CSS via le JS
+  waterDrop.style.fontSize = Math.random() * 7 + "px";
+  waterDrop.style.animationDuration = Math.random() * 2 + "s";
+  waterDrop.style.opacity = Math.random() + 0.3;
 
-    // Réparti les gouttes sur la largeur
-    // window.innerWidth = largeur de la window
-    waterDrop.style.left = Math.random() * window.innerWidth + 'px';
+  // Réparti les gouttes sur la largeur
+  // window.innerWidth = largeur de la window
+  waterDrop.style.left = Math.random() * window.innerWidth + "px";
 
-    // Intègre les gouttes dans la fontion
-    // créer l'enfant waterDrop à la balise body (toutes les 0.001sec) 
-    body.appendChild(waterDrop);
+  // Intègre les gouttes dans la fontion
+  // créer l'enfant waterDrop à la balise body (toutes les 0.001sec)
+  body.appendChild(waterDrop);
 
-
-    // tuer chaque élément qui a été créer (waterDrop), toutes les 6sec
-    setTimeout(() => { 
-        waterDrop.remove();
-    }, 6000)
-
-
+  // tuer chaque élément qui a été créer (waterDrop), toutes les 6sec
+  setTimeout(() => {
+    waterDrop.remove();
+  }, 6000);
 }
